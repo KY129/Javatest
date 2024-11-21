c5936476f49c7b690f46e3dfe29de1319e79c7af
KY129 <dlruddlf129@naver.com>
2024-11-21 18:18:32 +0900

    자바 테스트 문제

---
+++ Study002.java
@@ -0,0 +1,16 @@
+public class Study002 {
+    
+    public static void main(String[] args) {
+
+        String a = "자바";
+        String b = new String("자바");
+        if(a == b) {
+            System.out.println("참");
+        } else {
+            System.out.println("거짓");
+        }
+
+
+    }
+
+}
---
+++ Study003.java
@@ -0,0 +1,17 @@
+public class Study003 {
+    public static void main(String[] args) {
+
+        for(int 단 = 1; 단 <= 9; 단++) {
+            if(단 % 2 == 0) { 
+                System.out.println(단 + "단");
+                for(int i = 1 ; i <= 9; i++) {
+                    System.out.println(단 + "*" + i + "=" + (단 * i) + "\t");
+                        
+                    }   
+                    System.out.println();
+                }
+            }
+        }
+    }
+    
+
